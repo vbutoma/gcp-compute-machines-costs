794eb006d84c87fd832ca549c64f76b369b166f5
Vitaly <vit.butoma@gmail.com>
2025-05-21 00:39:46 +0300

    Fix loading GCPMachineType models

--- gcp_compute_machines/providers/base/models/base_machine_info_model.py
+++ gcp_compute_machines/providers/base/models/base_machine_info_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from gcp_compute_machines.providers.base.models.helper_types import IntOrNone, FloatOrNone
 
 class GCPMachineType(BaseModel):
@@ -38,6 +38,8 @@ class GCPMachineType(BaseModel):
     region: str | None = None
 
     # endregion
+
+    model_config = ConfigDict(populate_by_name=True)
 
 
 __all__ = [
